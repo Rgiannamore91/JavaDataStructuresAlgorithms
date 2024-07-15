1dd39f7c948679f1dc21ddf159aff904d9bdcaad
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-07-15 16:05:47 -0400

    successfully checks if names are palindromes.

--- DataStructures/LinkedLists/Palindrome/Palindrome/src/Main.java
+++ DataStructures/LinkedLists/Palindrome/Palindrome/src/Main.java
@@ -2,8 +2,28 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] names = {
+				"ava",
+				"elle",
+				"steve",
+				"hannah",
+				"chandler",
+				"sam",
+				"eve",
+				"gaylord",
+		};
+		for (String i : names) {
+			LinkedList nameList = nameToList(i);
+			isPalindrome(i, nameList);
+		}
+	}
+	
+	public static LinkedList nameToList(String name ){
+		LinkedList nameList = new LinkedList();
+		for (char i : name.toCharArray()) {
+			nameList.addToTail(i);
+		}
+		return nameList;
 	}
 	public static LinkedList reverse(LinkedList list) {
 		LinkedList reversed = new LinkedList(); 
@@ -12,5 +32,21 @@ public class Main {
 		}
 		return reversed;
 	}
-
+	public static String listToName(LinkedList list) {
+		String name ="";
+		while (list.head != null) {
+			name += list.removeHead();
+		}
+		return name;
+	}
+	public static void isPalindrome(String name, LinkedList nameList) {
+		LinkedList reverseNameList = reverse(nameList);
+		String reverseName = listToName(reverseNameList);
+		if (name.equals(reverseName)) {
+			System.out.println(name + " is a palindrome.");
+		}else {
+			System.out.println(name + " is not a palindrome.");
+		}
+	}
 }
+
