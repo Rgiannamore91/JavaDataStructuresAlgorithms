5219c7fe3ca50ae0d19126e021fce6ec3b84434b
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-07-24 11:56:44 -0400

    Created a program that uses a stack to reverse a queue.

---
+++ DataStructures/Stacks/ReverseQueue/reverseQueue/src/reverseQueue/Node.java
@@ -0,0 +1,17 @@
+package reverseQueue;
+public class Node {
+	public int data;
+	private Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public void setNextNode(Node node) {
+		this.next = node;
+	}
+	public Node getNextNode() {
+		return this.next;
+	}
+}
