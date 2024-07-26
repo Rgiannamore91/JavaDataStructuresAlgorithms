93f8d6d3b8f6a905c728ca198aa7d18918a3f936
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-07-26 15:54:50 -0400

    Created new stock data structures for node, linked list, queue, stack, and hash map that each allow dynamic typing.

---
+++ DataStructures/StockDataStructures/stockDataStructures/src/Queue.java
@@ -0,0 +1,50 @@
+
+public class Queue <T>{
+	public LinkedList <T, ?> queue;
+	public int size;
+	static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+	public int maxSize;
+	
+	public Queue() {
+		this(DEFAULT_MAX_SIZE);
+	}
+	
+	public Queue(int maxSize) {
+		this.queue = new LinkedList<>();
+		this.size = 0;
+		this.maxSize = maxSize;
+	}
+	
+	public boolean hasSpace() {
+		return this.size < this.maxSize;
+	}
+	
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+	
+	public void enqueue(T data) {
+		if (this.hasSpace()) {
+			this.queue.addToTail(data);
+			this.size++;
+		}else {
+			throw new Error("Queue is full!");
+		}
+	}
+	public T dequeue() {
+		if (!this.isEmpty()) {
+			T data = this.queue.removeHead();
+			this.size--;
+			return data;
+		}else {
+			throw new Error("Queue is empty!");
+		}
+	}
+	public T peek() {
+		if (this.isEmpty()) {
+			return null;
+		}else {
+			return this.queue.head.data;
+		}
+	}
+}
